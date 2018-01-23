84a23f5500369de694251dba2e2d5df6e64900b6
Andrzej Goławski <a.golawski@polish-airports.com>
2018-01-24 00:51:02 +0100

    simple application

---
+++ src/main/java/pl/osec/openshift/ConfigurationExampleApplication.java
@@ -0,0 +1,13 @@
+package pl.osec.openshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class ConfigurationExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(ConfigurationExampleApplication.class);
+        application.run(args);
+    }
+}
